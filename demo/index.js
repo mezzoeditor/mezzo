79705d49c65552c6f4f3b99f503ad5b13cd5a4ee
Dmitry Gozman <dgozman@chromium.org>
2017-11-18 18:29:37 -0800

    fix: overlay css

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,5 @@
 import { Editor } from "../src/Editor.mjs";
+import { Cursor } from "../src/Cursor.mjs";
 
 const editor = new Editor(document);
 document.body.appendChild(editor.element());
@@ -7,3 +8,7 @@ editor.element().style.height = '400px';
 editor.resize();
 editor.setText('short\nlonggggggggg\nshort\nlonggggggggg');
 editor.focus();
+
+// hack:
+let cursor = new Cursor({lineNumber: 2, columnNumber: 2});
+editor._renderer.invalidate(editor._text.addCursor(cursor));
