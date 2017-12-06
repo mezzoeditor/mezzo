5f31211b4eca503b67c7918c959c0d9922903663
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-06 01:59:17 -0800

    chore(demo): use shakespear as a stub text for demo content

--- demo/index.js
+++ demo/index.js
@@ -7,16 +7,20 @@ editor.element().style.width = '100%';
 editor.element().style.height = '100%';
 editor.resize();
 window.onresize = () => editor.resize();
-let text = [];
-for (let i = 0; i < 100; i++)
-  text.push('short\nlonggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg\n');
-editor.setText(text.join(''));
-editor.focus();
 
-let selections = [];
-for (let i = 0; i < 20; i++) {
-  let selection = new Selection();
-  selection.setCaret({lineNumber: 4 * i, columnNumber: 3});
-  selections.push(selection);
+setupEditor();
+
+async function setupEditor() {
+  const response = await fetch('shakespeare.txt');
+  const text = await response.text();
+  editor.setText(text);
+  editor.focus();
+
+  let selections = [];
+  for (let i = 0; i < 20; i++) {
+    let selection = new Selection();
+    selection.setCaret({lineNumber: 4 * i, columnNumber: 3});
+    selections.push(selection);
+  }
+  editor.setSelections(selections);
 }
-editor.setSelections(selections);
