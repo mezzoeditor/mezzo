07dcfc25ec695c81bac26f06eb9aa50aba2b441d
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-18 20:12:14 -0800

    demo: option to disable token highlight

--- demo/index.js
+++ demo/index.js
@@ -24,7 +24,7 @@ function addHighlights(editor) {
   const tokenHighlighter = new TokenHighlighter(editor);
 
   const select = document.querySelector('.highlights');
-  const highlights = ['the', 'e', '('];
+  const highlights = ['the', 'e', '(', ''];
   for (const highlight of highlights) {
     const option = document.createElement('option');
     option.textContent = highlight;
