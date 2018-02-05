fc9d67298264ee30e52c55c34c1ed6e52ba374fc
Andrey Lushnikov <lushnikov@chromium.org>
2018-02-04 21:23:29 -0500

    feature: store and reuse speculative highlight
    This patch starts storing and re-querying previously generated
    speculative highlight.
    
    This makes javascript syntax very non-blinking.

--- src/syntax/javascript.mjs
+++ src/syntax/javascript.mjs
@@ -1,12 +1,16 @@
 import { Decorator } from "../core/Decorator.mjs";
 import {Parser, TokenTypes, KeywordTypes} from './jslexer/index.mjs';
-import {TextUtils} from "../utils/TextUtils.mjs";
 
 /**
  * @interface
  */
 export default class {
   constructor() {
+    this._speculativeHighlight = new Decorator();
+  }
+
+  onReplace(from, to, inserted) {
+    this._speculativeHighlight.onReplace(from, to, inserted);
   }
 
   /**
@@ -16,24 +20,33 @@ export default class {
    */
   onFrame(frame) {
     let decorator = new Decorator();
-    for (let range of frame.ranges())
-      tokenizeText(frame.document().iterator(range.from, range.from, range.to), decorator);
+    for (let range of frame.ranges()) {
+      let from = range.from;
+      let decoration = this._speculativeHighlight.lastTouching(from, from);
+      if (decoration) {
+        decorator.add(from, decoration.to, decoration.style);
+        from = decoration.to;
+      }
+
+      this._speculativeHighlight.clearTouching(from + 1, range.to);
+      let iterator = frame.document().iterator(from, from, range.to);
+      let tt = new Parser({allowHashBang: true}, iterator);
+      for (let token of tt) {
+        if (token.type.keyword || (token.type === TokenTypes.name && token.value === 'let')) {
+          decorator.add(token.start, token.end, 'syntax.keyword');
+        } else if (token.type === TokenTypes.string || token.type === TokenTypes.regexp || token.type === TokenTypes.template || token.type === TokenTypes.invalidTemplate) {
+          decorator.add(token.start, token.end, 'syntax.string');
+          this._speculativeHighlight.add(token.start, token.end, 'syntax.string');
+        } else if (token.type === TokenTypes.num) {
+          decorator.add(token.start, token.end, 'syntax.number');
+        } else if (token.type === TokenTypes.blockComment || token.type === TokenTypes.lineComment) {
+          decorator.add(token.start, token.end, 'syntax.comment');
+          this._speculativeHighlight.add(token.start, token.end, 'syntax.comment');
+        } else {
+          decorator.add(token.start, token.end, 'syntax.default');
+        }
+      }
+    }
     return [decorator];
   }
 };
-
-function tokenizeText(text, decorator) {
-  let tt = new Parser({allowHashBang: true}, text);
-  for (let token of tt) {
-    if (token.type.keyword || (token.type === TokenTypes.name && token.value === 'let'))
-      decorator.add(token.start, token.end, 'syntax.keyword');
-    else if (token.type === TokenTypes.string || token.type === TokenTypes.regexp || token.type === TokenTypes.template || token.type === TokenTypes.invalidTemplate)
-      decorator.add(token.start, token.end, 'syntax.string');
-    else if (token.type === TokenTypes.num)
-      decorator.add(token.start, token.end, 'syntax.number');
-    else if (token.type === TokenTypes.blockComment || token.type === TokenTypes.lineComment)
-      decorator.add(token.start, token.end, 'syntax.comment');
-    else
-      decorator.add(token.start, token.end, 'syntax.default');
-  }
-}
