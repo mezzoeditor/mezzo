f8eaea0826a10981159ad0bf56194f7e3d6ccd7f
Andrey Lushnikov <lushnikov@chromium.org>
2018-03-11 14:13:01 +0400

    chore(jslexer): remove unused reserved words from lexer

--- src/javascript/jslexer/identifier.mjs
+++ src/javascript/jslexer/identifier.mjs
@@ -1,13 +1,3 @@
-// Reserved word lists for various dialects of the language
-
-export const reservedWords = {
-  3: "abstract boolean byte char class double enum export extends final float goto implements import int interface long native package private protected public short static super synchronized throws transient volatile",
-  5: "class enum extends super const export import",
-  6: "enum",
-  strict: "implements interface let package private protected public static yield",
-  strictBind: "eval arguments"
-}
-
 // And the keywords
 
 const ecma5AndLessKeywords = "break case catch continue debugger default do else finally for function if return switch throw try var while with null true false instanceof typeof void delete new in this"
