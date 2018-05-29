8ec700721a44565dbe54e143e4beae8dded569f4
Andrey Lushnikov <lushnikov@chromium.org>
2018-05-29 00:34:48 -0700

    fix(web): teach WebPlatformSupport to work without requestIdleCallback
    E.g. Safari doesn't have requestIdleCallback; this patch
    makes it not a problem.

--- src/web/WebPlatformSupport.mjs
+++ src/web/WebPlatformSupport.mjs
@@ -1,18 +1,28 @@
 let platformSupport = null;
 
 export class WebPlatformSupport {
+  constructor() {
+    if (window.requestIdleCallback) {
+      this._requestIdleCallback = callback => window.requestIdleCallback(callback.bind(null), {timeout: 1000});
+      this._cancelIdleCallback = window.cancelIdleCallback.bind(window);
+    } else {
+      this._requestIdleCallback = window.requestAnimationFrame.bind(window);
+      this._cancelIdleCallback = window.cancelAnimationFrame.bind(window);
+    }
+  }
+
   /**
    * @return {number}
    */
   requestIdleCallback(callback) {
-    return window.requestIdleCallback(() => callback(), {timeout: 1000});
+    return this._requestIdleCallback.call(null, callback);
   }
 
   /**
    * @param {number} callbackId
    */
   cancelIdleCallback(id) {
-    window.cancelIdleCallback(id);
+    this._cancelIdleCallback.call(null, id);
   }
 
   static instance() {
