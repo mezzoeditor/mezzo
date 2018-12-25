d18fe0df42052cf03620e4aa1158af9929468391
Andrey Lushnikov <lushnikov@chromium.org>
2018-12-25 11:31:18 +0100

    feature(lang): markdown mode to support embedded CSS and JS

--- lang/markdown/MDHighlighter.mjs
+++ lang/markdown/MDHighlighter.mjs
@@ -1,6 +1,8 @@
 import {CMHighlighter} from '../codemirror/CMHighlighter.mjs';
 import {} from '../codemirror/modes/overlay.js';
 import {} from '../codemirror/modes/xml.js';
+import {} from '../codemirror/modes/css.js';
+import {} from '../codemirror/modes/javascript.js';
 import {} from '../codemirror/modes/markdown.js';
 import {} from '../codemirror/modes/gfm.js';
 
@@ -11,6 +13,17 @@ export async function createHighlighter(editor) {
     'number': 'syntax.number',
     'comment': 'syntax.comment',
     'variable-2': 'syntax.variable',
+    'css-property': 'syntax.string',
+    'css-meta': 'syntax.string',
+    'css-atom': 'syntax.keyword',
+    'css-number': 'syntax.number',
+    'css-comment': 'syntax.comment',
+    'css-variable-2': 'syntax.variable',
+    'css-string': 'syntax.string',
+    'js-keyword': 'syntax.keyword',
+    'js-string': 'syntax.string',
+    'js-number': 'syntax.number',
+    'js-comment': 'syntax.comment',
   })));
 }
 
